45148f7df054391ccb9c0250c396aab6f34ba928
pramayu <gedepramayu@gmail.com>
2016-12-02 09:08:43 +0700

    cloudinary

--- app/uploaders/slider_uploader.rb
+++ app/uploaders/slider_uploader.rb
@@ -4,12 +4,15 @@ class SliderUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  process :tags => ["photo_album_sample"]
+  process :convert => "png"
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -48,17 +51,10 @@ class SliderUploader < CarrierWave::Uploader::Base
   version :large do 
     process :resize_to_fill => [1920, 1280]
   end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+ 
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
+  
 end
